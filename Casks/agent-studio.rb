4a1810e16639135fd615fc4505e7e10be942106d
Shravan Sunder <ShravanSunder@users.noreply.github.com>
2026-03-25 22:18:48 -0400

    Update agent-studio to v0.0.23

--- Casks/agent-studio.rb
+++ Casks/agent-studio.rb
@@ -1,6 +1,6 @@
 cask "agent-studio" do
-  version "0.0.16"
-  sha256 "4f8e55954c3213933820a783e12f603f422a1ad238970a3e83d1d5ba132b432d"
+  version "0.0.23"
+  sha256 "3f70b1c41e85d3cf77eb234b1f5504a6b8526f2151d2eba936ad3b0f135a6c94"
 
   url "https://github.com/ShravanSunder/agentstudio/releases/download/v#{version}/AgentStudio-v#{version}-macos.zip"
   name "Agent Studio"
